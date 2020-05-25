ed6044a9efaba0afa7f8cd36ef6a792c795b0eb7
RoberaGeleta <roberageleta@gmail.com>
2020-05-25 16:58:45 -0700

    Initial Commit

---
+++ RGStack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGStack
+//
+//  Created by ROBERA GELETA on 5/25/20.
+//  Copyright © 2020 ROBERA GELETA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
